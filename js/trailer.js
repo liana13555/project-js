548cc776f402ff14264861631edd5f9c31fa0a35
Liana Baryshova <barishovaliana@gmail.com>
2021-08-14 15:05:34 +0200

    Update file

--- js/trailer.js
+++ js/trailer.js
@@ -1,9 +1,7 @@
 // import QueryService from './query'
 const KEY_USER = '2fa9e4bbaa008ede70ee7a4ceca0d3a2';
 
-const url = 'https://api.themoviedb.org/3/search/movie?api_key=2fa9e4bbaa008ede70ee7a4ceca0d3a2&query=furious';
-const BASE_URL = 'https://api.themoviedb.org/3/';
-
+const url = 'https://api.themoviedb.org/3/search/movie?api_key=2fa9e4bbaa008ede70ee7a4ceca0d3a2';
 
 
 const watchTrailer = document.querySelector('#trailer');
@@ -13,5 +11,36 @@ watchTrailer.addEventListener('click', onClick);
 
 
 function onClick(event) {
+    event.preventDefault();
+    const value = input.value;
+
+    const path = '/search/movie';
+    const newUrl = generateUrl(path) +'&query=' + value;
    
+    fetch(newUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Data:', data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    console.log('Value:', value);
+}
+
+function generateUrl(path) {
+    const url = `https://api.themoviedb.org/3${path}?api_key=2fa9e4bbaa008ede70ee7a4ceca0d3a2`;
+
+    return url;
+}
+
+function createIframe(video) {
+    const iframe = document.createElement('iframe');
+    iframe.src = `https://www.youtube.com/embed/${video.key}`;
+    iframe.width = 360;
+    iframe.height = 315;
+
+    iframe.allowFullscreen = true;
+    return iframe;
+    
 }
